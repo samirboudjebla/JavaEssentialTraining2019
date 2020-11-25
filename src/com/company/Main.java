335ba0979c3cccedace7b1285bf43bd631334b78
samboudjebla <samboudjebla@hotmail.com>
2020-11-24 23:12:02 -0800

    added : added primitive values and conversions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number :");
+        double firstValue = scanner.nextDouble();
+
+        System.out.print("Enter the second number :");
+        double secondValue = scanner.nextDouble();
+
+        double  sum = firstValue/secondValue;
+        System.out.println("The result of the division of is : " +sum);
+
+
+
     }
 }

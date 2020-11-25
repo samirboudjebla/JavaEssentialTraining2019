7643ba1a776576a9807215f576ba2aa97780a5d8
samboudjebla <samboudjebla@hotmail.com>
2020-11-24 22:33:49 -0800

    added : added primitive values and conversions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.text.NumberFormat;
+
 public class Main {
 
     public static void main(String[] args) {
         var doubleValue = 10_000_000.53;
+
+        var numF = NumberFormat.getNumberInstance();
+        System.out.println("Number Format: "+ numF.format(doubleValue));
+
+        var intF = NumberFormat.getIntegerInstance();
+        System.out.println("Integer Format: "+ intF.format(doubleValue));
+
+        intF.setGroupingUsed(false);
+        System.out.println("Integer Format: "+ intF.format(doubleValue));
+
+
     }
 }

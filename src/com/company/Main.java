b6e23299ca5ae7e3b3947c98b599faba870d3e2b
samboudjebla <samboudjebla@hotmail.com>
2020-11-24 23:07:10 -0800

    added : added primitive values and conversions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+
+        System.out.print("Enter a value: ");
+        var input = scanner.nextLine();
+        System.out.println("Entered value : " + input);
+
+        System.out.print("Enter number 1: ");
+        var number1 = scanner.nextInt();
+
+        System.out.print("Enter number 2: ");
+        var number2 = scanner.nextInt();
+
+        var sum = number1+number2;
+        System.out.println("the sum of number 1 + number 2 is : "+ sum);
+
 
     }
 }

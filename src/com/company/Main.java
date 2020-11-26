b56e7dab7994e1d1f80dbe469425472753fefe5e
samboudjebla <samboudjebla@hotmail.com>
2020-11-26 13:08:09 -0800

    added : added primitive and if conditions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,5 +10,25 @@ public class Main {
         System.out.print("Enter a month number (1-12): ");
         var monthNumber = scanner.nextInt();
 
+        String message;
+        if (monthNumber<1 || monthNumber>12){
+            message = "please enter a valid month!";
+        }else {
+            if (monthNumber<=3){
+                message = "First Quarter";
+            }else{
+                if (monthNumber<=6){
+                    message = "Second Quarter";
+                }else{
+                    if (monthNumber<=9){
+                        message = "Third Quarter";
+                    }else{
+                        message = "Fourth Quarter";
+                    }
+                }
+            }
+        }
+        System.out.println(message);
+
     }
 }

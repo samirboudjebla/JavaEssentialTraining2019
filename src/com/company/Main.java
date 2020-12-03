7665a556205c9a8113fd93795cb6aff8af41b398
samboudjebla <samboudjebla@hotmail.com>
2020-12-03 13:04:59 -0800

    exception handling

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String s = null;
+
+        try {
+            var sub = s.substring(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("i'm dead yet");
+
+
     }
 }

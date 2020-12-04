5846dbf1ef5e6574e13c924205ab0a85f2a37262
samboudjebla <samboudjebla@hotmail.com>
2020-12-03 18:27:46 -0800

    Added a sub class Hat and implemented a polymorphism private method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,8 @@
 package com.company;
 
+import com.company.model.ClothingItem;
 import com.company.model.ClothingSize;
+import com.company.model.Hat;
 import com.company.model.Shirt;
 
 import java.text.NumberFormat;
@@ -9,11 +11,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        var item = new Shirt(
+        var shirt = new Shirt(
                 ClothingSize.L,
                 19.99,
                 3);
 
+        displayItemDetails(shirt);
+        
+        var hat = new Hat(ClothingSize.M, 29.99, 1);
+        displayItemDetails(hat);
+    }
+
+    private static void displayItemDetails(ClothingItem item) {
         var totalPrice = item.getPrice() * item.getQuantity();
         var formatter = NumberFormat.getCurrencyInstance();
         var output = String.format("Your %s %s order will cost %s",
@@ -22,4 +31,5 @@ public class Main {
                 formatter.format(totalPrice));
         System.out.println(output);
     }
+
 }

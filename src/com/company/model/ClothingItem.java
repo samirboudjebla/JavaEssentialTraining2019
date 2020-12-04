f31ac61f6a42059b45d956c7ea1075023d6e878c
samboudjebla <samboudjebla@hotmail.com>
2020-12-03 16:57:24 -0800

    created constructor

--- src/com/company/model/ClothingItem.java
+++ src/com/company/model/ClothingItem.java
@@ -6,6 +6,16 @@ public class ClothingItem {
     private double price;
     private int quantity;
 
+/*    public ClothingItem() {
+    }*/
+
+    public ClothingItem(String type, String size, double price, int quantity) {
+        this.type = type;
+        this.size = size;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
     public String getType() {
         return type;
     }
@@ -37,4 +47,7 @@ public class ClothingItem {
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
+
+
+
 }
